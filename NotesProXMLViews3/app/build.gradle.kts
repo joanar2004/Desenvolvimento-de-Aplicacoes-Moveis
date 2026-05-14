3a87b1ae9b379fb0c8f14b9b24569079db21db12
limar <limaramos.joana@gmail.com>
2026-05-15 00:03:27 +0100

    as notas já são mostradas na página principal

--- NotesProXMLViews3/app/build.gradle.kts
+++ NotesProXMLViews3/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.firestore)
+    implementation("com.firebaseui:firebase-ui-firestore:9.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
